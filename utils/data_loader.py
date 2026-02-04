fix the dual normalization issue

utils/data_loader.py
@@ -79,7 +79,7 @@ class TeaLeafDataset(Dataset):
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = cv2.resize(image, self.image_size)
-        image = image.astype(np.float32) / 255.0
+        #image = image.astype(np.float32) / 255.0
         if self.transform:
             image = self.transform(image=image)['image']
         return image, label